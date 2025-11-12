f7ec82085520f265ca3e5a5bc7170306a2999fba
BigALL-IN <vladcroitor.official@gmail.com>
2025-11-12 15:26:03 +0200

    lab07
    ex1

---
+++ Lab07/ex1.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pymc as pm
+import arviz as az
+import matplotlib.pyplot as plt
+
+def main():
+
+    data = np.array([56, 60, 58, 55, 57, 59, 61, 56, 58, 60])
+    x_bar = data.mean()
+    print(f"Sample mean = {x_bar}, Sample std = {data.std(ddof=1)}")
+    # a, b)
+    with pm.Model() as weak_model:
+        mu = pm.Normal("mu", mu=x_bar, sigma=10)
+        sigma = pm.HalfNormal("sigma", sigma=10)
+        y = pm.Normal("y", mu=mu, sigma=sigma, observed=data)
+
+        trace_weak = pm.sample(2000, tune=2000, target_accept=0.9, random_seed=42)
+        summary_weak = az.summary(trace_weak, var_names=["mu", "sigma"], hdi_prob=0.95)
+
+    print("\nPosterior summaries (Weak Prior):")
+    print(summary_weak)
+
+    # 4. Compare with frequentist estimates
+    # c)
+    print("\nFrequentist estimates:")
+    print(f"Mean: {np.mean(data)}")
+    print(f"SD:   {np.std(data, ddof=1)}")
+    print("The Bayesian and frequentist estimates are very similar, thanks to a weak prior and sufficient data, the small difference is caused by bayes being influenced by the small prior")
+    # d)
+    with pm.Model() as strong_model:
+        mu = pm.Normal("mu", mu=50, sigma=1)
+        sigma = pm.HalfNormal("sigma", sigma=10)
+        y = pm.Normal("y", mu=mu, sigma=sigma, observed=data)
+
+        trace_strong = pm.sample(2000, tune=2000, target_accept=0.9, random_seed=42)
+        summary_strong = az.summary(trace_strong, var_names=["mu", "sigma"], hdi_prob=0.95)
+
+    print("\nPosterior summaries (Strong Prior):")
+    print(summary_strong)
+
+    az.plot_posterior(trace_weak, var_names=["mu", "sigma"], hdi_prob=0.95)
+    plt.suptitle("Posterior with Weak Prior", fontsize=14)
+    plt.show()
+
+    az.plot_posterior(trace_strong, var_names=["mu", "sigma"], hdi_prob=0.95)
+    plt.suptitle("Posterior with Strong Prior", fontsize=14)
+    plt.show()
+
+    print("The strong prior shifts the HDI towards 50dB, while in the case of the weak prior sigma mainly relies on data, since HalfNormal(10) is broad")
+
+if __name__ == "__main__":
+    main()
